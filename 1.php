2329558e5ac9c9a710a8ed87d98f2f09c2357046
User <User@WIN-HJNK92FS7ME>
2017-09-23 19:08:02 +0300

    add id="main"

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		<div>
+			<div id="main">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, natus.</div>
+			<div id="main">Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, hic.</div>
+			<div id="main">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, necessitatibus.</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
