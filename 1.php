917afca441451e11bde9cb247f41e84e415bc738
User <User@WIN-HJNK92FS7ME>
2017-09-23 19:06:07 +0300

    update file 1.php

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div>
+		<div>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quaerat, natus.</div>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, hic.</div>
+			<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, necessitatibus.</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
